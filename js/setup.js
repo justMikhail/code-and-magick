96bcb6b19d80b7ae52a03954defc6b3ad4714b90
justMikhail <mikhail.walle@gmail.com>
2020-10-09 21:23:07 +0500

    Улучшил читаемость кода

--- js/setup.js
+++ js/setup.js
@@ -1,24 +1,22 @@
 'use strict';
 
-// Обьявляет КОНСТАНТЫ====================================================
+// Обьявляет КОНСТАНТЫ-----------------------------------------------------------------------------------
 const WIZARDS_QUANTITY = 4;
 const WIZARD_NAMES = [`Иван`, `Хуан Себастьян`, `Мария`, `Кристоф`, `Виктор`, `Юлия`, `Люпита`, `Вашингтон`];
 const WIZARD_SURNAMES = [`да Марья`, `Верон`, `Мирабелла`, `Вальц`, `Онопко`, `Топольницкая`, `Нионго`, `Ирвинг`];
 const COAT_COLORS = [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`, `rgb(56, 159, 117)`, `rgb(215, 210, 55)`, `rgb(0, 0, 0)`];
 const EYES_COLORS = [`black`, `red`, `blue`, `yellow`, `green`];
 
-// Показывает блок с карточкой мага=======================================
 const userDialog = document.querySelector(`.setup`);
 userDialog.classList.remove(`hidden`);
 
-// =======================================================================
 const similarListElement = document.querySelector(`.setup-similar-list`);
 
 const similarWizardTemplate = document.querySelector(`#similar-wizard-template`)
     .content
     .querySelector(`.setup-similar-item`);
 
-// Функция возвращает случайный элемент масива============================
+// Генерируется случайный элемент масива------------------------------------------------------------------
 function getRandom(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
@@ -26,10 +24,10 @@ const getRandomArrayElement = (array) => {
   return array[getRandom(array.length - 1)];
 };
 
-// Создает новый пустой DocumentFragment=================================
+// Создается новый пустой Фрагмент------------------------------------------------------------------------
 const fragment = document.createDocumentFragment();
 
-// Функция создает обьект с данными мага
+// Создается обьект с данными мага
 const getDataWizard = function () {
   return {
     wizardName: `${getRandomArrayElement(WIZARD_NAMES)} ${getRandomArrayElement(WIZARD_SURNAMES)}`,
@@ -38,7 +36,7 @@ const getDataWizard = function () {
   };
 };
 
-// Функция генерирует случайных волшебников==============================
+// Генерируются случайные волшебники----------------------------------------------------------------------
 const renderWizards = function () {
   for (let i = 0; i < WIZARDS_QUANTITY; i++) {
 
